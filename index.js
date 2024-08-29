ee4c2de2b8f4984187c60fefc377953bbbbac3cd
Md. Parvaj Mosharof <mdparvajmosharof17.com>
2024-08-29 07:53:28 +0600

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const cors = require("cors");
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -35,7 +36,27 @@ async function run() {
     const bannerCollection = client.db("testsdb").collection("banner");
     const bookedCollection = client.db("testsdb").collection("booked");
     
-    
+    //jwt 
+    app.post("/jwt", async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send({token})
+    })
+
+    //midleware
+    const verifyToken = (req, res, next)=>{
+      if(!req.headers.authorization){
+        return res.status(401).send({message  : "unauthorized access"});
+       }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message  : "unauthorized access"});
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
 
     // Get all tests
     app.get('/tests', async(req, res) => {
@@ -86,7 +107,7 @@ async function run() {
 
     //get user
 
-    app.get("/users", async(req, res)=> {
+    app.get("/users",verifyToken , async(req, res)=> {
       const users = req.body
       const result = await usersCollection.find(users).toArray()
       res.send(result)
